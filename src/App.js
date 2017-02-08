a535163cdef94a8b1c701d02673ef2fea6d23c91
Pierre Ferreira <pierre@tektite.biz>
2017-02-08 07:14:41 +0200

    Boilerplate, Redux, Firebase setup and installs,

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers';
+
+class App extends Component {
+  componentWillMount() {
+    // Initialize Firebase
+    const config = {
+      apiKey: 'AIzaSyDTx0HmTQcbuf16fZNEXzYeqL-k0-xBZJw',
+      authDomain: 'gsrnmanager.firebaseapp.com',
+      databaseURL: 'https://gsrnmanager.firebaseio.com',
+      storageBucket: 'gsrnmanager.appspot.com',
+      messagingSenderId: '445822190336'
+    };
+    firebase.initializeApp(config);
+  }
+  
+  render() {
+    return (
+      <Provider store={createStore(reducers)}>
+        <View>
+          <Text>
+            RNMANAGER
+          </Text>
+        </View>
+      </Provider>
+    );
+  }
+}
+
+export default App;
