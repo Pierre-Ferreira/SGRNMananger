4ef9368045bf4e0367746d374e27edd0e87295f0
Pierre Ferreira <pierre@tektite.biz>
2017-02-10 01:32:44 +0200

    Routing #2

--- src/Router.js
+++ src/Router.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import { Scene, Router } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
+import EmployeeList from './components/EmployeeList';
 
 const RouterComponent = () => {
   return (
-    <Router>
-      <Scene
-        key="login"
-        component={LoginForm}
-        title="Please login"
-      />
-
+    <Router sceneStyle={{ paddingTop: 65 }}>
+      <Scene key='auth'>
+        <Scene
+          key="login"
+          component={LoginForm}
+          title="Please login"
+          initial
+        />
+      </Scene>
+      <Scene key='main'>
+        <Scene
+          key="employeeList"
+          component={EmployeeList}
+          title="Employee List"
+          rightTitle="ADD"
+          onRight={() => console.log("RIGHT!")}
+        />
+      </Scene>
     </Router>
   );
 };
