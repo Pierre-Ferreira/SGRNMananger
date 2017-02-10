67f7721f8023577a8a695cf0578e20b9a3969609
Pierre Ferreira <pierre@tektite.biz>
2017-02-10 02:56:07 +0200

    CreateEmployee form #1

--- src/Router.js
+++ src/Router.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import { Scene, Router } from 'react-native-router-flux';
+import { Scene, Router, Actions } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
+import EmployeeCreate from './components/EmployeeCreate';
 
 const RouterComponent = () => {
   return (
     <Router sceneStyle={{ paddingTop: 65 }}>
-      <Scene key='auth'>
-        <Scene
-          key="login"
-          component={LoginForm}
-          title="Please login"
-          initial
-        />
-      </Scene>
+
       <Scene key='main'>
         <Scene
           key="employeeList"
           component={EmployeeList}
           title="Employee List"
           rightTitle="ADD"
-          onRight={() => console.log("RIGHT!")}
+          onRight={() => Actions.employeeCreate()}
+
+        />
+        <Scene
+          key="employeeCreate"
+          component={EmployeeCreate}
+          title="Create Employee"
+          // rightTitle="ADD"
+          // onRight={() => Action.}
+        />
+      </Scene>
+      <Scene key='auth'>
+        <Scene
+          key="login"
+          component={LoginForm}
+          title="Please login"
+          initial
         />
       </Scene>
     </Router>
